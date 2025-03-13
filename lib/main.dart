a46dcbea82dcfdfa6af5a12538c3e12351220fc9
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-03-13 12:35:53 +0800

    Lists of Data and Custom Classes

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+//THIS IS FOR LISTS OF DATA TUTORIAL.
+void main() => runApp(MaterialApp(
+  home:QuoteList(),
+));
+//LISTS OF DATA.
+class QuoteList extends StatefulWidget {
+  
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken.'),
+    Quote(author: 'Mae West', text: 'You only live once, but if you do it right, once is enough.'),
+    Quote(author: 'Oscar Wilde', text: 'To live is the rarest thing in the world. Most people exist, that is all.'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      )
+    );
+  }
+}
---
+++ lib/quote.dart
@@ -0,0 +1,10 @@
+class Quote {
+
+  String? text;
+  String? author;
+
+  Quote({ this.text, this.author });
+
+}
+
+Quote myquote = Quote(author: 'Oscar wilde', text: 'This is the quote text',);
